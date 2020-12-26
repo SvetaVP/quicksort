5d7c41b79d2bee7fe6d8ac57eabedeaf783f186d
Sveta <qwe7vbn12@gmail.com>
2020-12-26 22:41:10 +0200

    replaced let with const

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const numbers = [9, 6, 1, 2, 4, 10, -3, 15, 7, 3, 8, 0, 5];
 console.log(sort(numbers));
 
 function sort(items) {
-  if (items.length == 0) return [];
+  if (!items.length) return [];
 
-  let lessPivot = [];
-  let morePivot = [];
-  let pivot = items[0];
+  const lessPivot = [];
+  const morePivot = [];
+  const pivot = items[0];
 
-  for (var i = 1; i < items.length; i++) {
+  for (let i = 1; i < items.length; i++) {
     if (items[i] < pivot) lessPivot[lessPivot.length] = items[i];
     else morePivot[morePivot.length] = items[i];
   }
